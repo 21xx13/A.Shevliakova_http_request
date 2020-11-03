392400a440c5ef456a21b9a8d118b8686cac2229
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-11-04 00:29:43 +0500

    http get request

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("www.google.com", 80)) {
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(("GET / HTTP/1.1\n" +
+                    "Host: www.google.com\n" +
+                    "Accept: text/html\n" +
+                    "Connection: close\r\n\r\n").getBytes());
+            outputStream.flush();
+            Files.createFile(Paths.get("src/google.html"));
+            FileWriter fw = new FileWriter("src/google.html");
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            int read = in.read();
+            while (read > 0) {
+                System.out.print((char) read);
+                read = in.read();
+                fw.write((char) read);
+            }
+            fw.close();
+        } catch (Exception ignored) {
+        }
+
+
+        /*
+        URL url = new URL("https://proselyte.net/tutorials/");
+        InputStream is = url.openStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+        int read = in.read();;
+        while (read > 0 ) {
+            System.out.print((char)read);
+            read = in.read();
+        } */
+    }
+}
