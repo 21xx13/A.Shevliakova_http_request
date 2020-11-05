0cf3212b6cb5481ab7fa4500e0397f34ff562bb5
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-11-05 20:23:26 +0500

    html (get request + URL)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,15 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("www.google.com", 80)) {
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("ai-info.ru", 80)) {
             OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(("GET / HTTP/1.1\n" +
-                    "Host: www.google.com\n" +
-                    "Accept: text/html\n" +
+            outputStream.write(("GET / HTTP/1.1\r\n" +
+                    "Host: ai-info.ru\r\n" +
                     "Connection: close\r\n\r\n").getBytes());
             outputStream.flush();
-            Files.createFile(Paths.get("src/google.html"));
-            FileWriter fw = new FileWriter("src/google.html");
+            Files.createFile(Paths.get("src/ai-info.html"));
+            FileWriter fw = new FileWriter("src/ai-info.html");
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             int read = in.read();
             while (read > 0) {
@@ -30,15 +29,17 @@ public class Main {
         } catch (Exception ignored) {
         }
 
-
-        /*
-        URL url = new URL("https://proselyte.net/tutorials/");
+        URL url = new URL("https://google.com");
         InputStream is = url.openStream();
+        Files.createFile(Paths.get("src/google.html"));
+        FileWriter fw = new FileWriter("src/google.html");
         BufferedReader in = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-        int read = in.read();;
-        while (read > 0 ) {
-            System.out.print((char)read);
+        int read = in.read();
+        while (read > 0) {
+            System.out.print((char) read);
             read = in.read();
-        } */
+            fw.write((char) read);
+        }
+        fw.close();
     }
 }
